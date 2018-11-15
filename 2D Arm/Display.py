c8e90f73e4c0ae649d7058fb9e7a54ec14467992
Magnus <jaloeffler97@gmail.com>
2018-11-14 22:04:20 -0800

    Added the ability to test multiple configs serially

--- 2D Arm/Display.py
+++ 2D Arm/Display.py
@@ -116,7 +116,7 @@ class DrawingWidget(QtGui.QWidget):
         
 print RobotArm.calculatePosition([100,100],[120,240])
 print calculateDistanceBetween2D((100,100), (200, 200))
-winningOrganism = Evolve(DISTANCES)
+winningOrganism = Evolve_Different_Paramters(DISTANCES)
         
 app = QtGui.QApplication(sys.argv)
 widget = DrawingWidget(winningOrganism)
--- 2D Arm/NEATEvolution.py
+++ 2D Arm/NEATEvolution.py
@@ -4,8 +4,11 @@ import random
 from RoboticArm2D import RobotArm
 import math
 import os
-
+import sys
 config_file = "config-feedforward.txt"
+
+generation = 0
+
 def Evolve(distances):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
     pop = neat.Population(config)
@@ -49,7 +52,28 @@ def Evolve(distances):
     
     return winnerOrganism
     
+def Evolve_Different_Paramters(distances):
+    global generation
+    config1 = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, "configurations/lowAddition_lowSubtraction_weights30.txt")
+    config2 = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, "configurations/lowAddition_lowSubtraction_weights1000.txt")
+    config3 = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, "configurations/lowAddition_noSubtraction_weights30.txt")
+    config4 = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, "configurations/lowAddition_noSubtraction_weights1000.txt")
     
+    configs = [config1, config2, config3, config4]
+    data = []
+    for index,con in enumerate(configs):
+        print "Running Config Number:", index
+        data.append([])
+        for i in range(10):
+            print "\n\nRunning Instance:", i
+            generation = 0
+            pop = neat.Population(con)
+            #pop.add_reporter(neat.StdOutReporter(True))
+            winner = pop.run(lambda geneomes, con: __calculateFitnessMovingPoint(geneomes, con, distances), 3000)
+            data[-1].append(winner.fitness)
+    
+    print data
+    sys.exit(0)
 
 
 def __calculateFitnessFixedPoint(geneomes, config, distances):
@@ -76,7 +100,9 @@ def __calculateFitnessFixedPoint(geneomes, config, distances):
         #genome.fitness = 1/ (float(abs(endEffectorPosition[0] - xPos) + abs(endEffectorPosition[1] - yPos)))
         
 def __calculateFitnessMovingPoint(geneomes, config, distances):
-    
+    global generation
+    print "\rGeneration:", generation,
+    generation += 1
     maximumDistance = 200
     numberOfTest = 10
     
